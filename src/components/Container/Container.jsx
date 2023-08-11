53cbea8dac5f6942042222e2d7dfaa0c98e84bd1
Md Hasanuzzaman <dr.has82@gmail.com>
2023-08-12 01:38:15 +0600

    Precius Section has done

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,6 +1,6 @@
 const Container = ({ children }) => {
     return (
-        <div className="max-w-[1200px] mx-auto px-10 py-5 w-full">
+        <div className="max-w-[1200px] mx-auto px-10 w-full">
             {children}
         </div>
     );
--- src/components/Precious/Precious.jsx
+++ src/components/Precious/Precious.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Container from '../Container/Container';
-import precius from "../../assets/precious/marriage.png"
+import { BsArrowRight } from "react-icons/bs";
+
+
 
 const Precious = () => {
     return (
-        <div>
+        <div className='pt-[60px]'>
             <Container>
-                <div className='flex'>
+                <div className='flex gap-10'>
                     <div className='w-1/2'>
-                        <img src={precius} alt="Abacus Convention Center" />
+                        <img src="https://raw.githubusercontent.com/Hasan082/abacus-convention-center-react/main/src/assets/precious%20/marriage.png" alt="Abacus Convention Center" />
                     </div>
-                    <div className='w-1/2'></div>
+                    <div className='w-1/2 space-y-4 inline-flex items-start flex-col justify-center'>
+                        <h2>Every moment is Precious</h2>
+                        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                        <a href="" className='inline-flex items-center gap-2 btn btn-primary'>Explore our album <BsArrowRight /> </a>
+
+                    </div>
                 </div>
             </Container>
         </div>
