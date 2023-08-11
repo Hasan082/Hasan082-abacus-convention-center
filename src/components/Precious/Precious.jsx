986c0c58e5a8ddfd673cbd15807ef4f24289ccc6
Md Hasanuzzaman <dr.has82@gmail.com>
2023-08-12 01:50:25 +0600

    Responsive done

--- src/components/Precious/Precious.jsx
+++ src/components/Precious/Precious.jsx
@@ -8,11 +8,11 @@ const Precious = () => {
     return (
         <div className='pt-[60px]'>
             <Container>
-                <div className='flex gap-10'>
-                    <div className='w-1/2'>
+                <div className='flex flex-col md:flex-row gap-10'>
+                    <div className='w-full md:w-1/2'>
                         <img src="https://raw.githubusercontent.com/Hasan082/abacus-convention-center-react/main/src/assets/precious%20/marriage.png" alt="Abacus Convention Center" />
                     </div>
-                    <div className='w-1/2 space-y-4 inline-flex items-start flex-col justify-center'>
+                    <div className='w-full md:w-1/2 space-y-4 inline-flex items-start flex-col justify-center'>
                         <h2>Every moment is Precious</h2>
                         <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                         <a href="" className='inline-flex items-center gap-2 btn btn-primary'>Explore our album <BsArrowRight /> </a>
